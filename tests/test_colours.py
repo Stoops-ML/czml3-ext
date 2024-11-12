1d26f7decd47d16f1bec73842a21590a4f4c1728
Daniel Stoops <dainelst@omnisys.co.il>
2024-11-12 10:56:41 +0200

    Fix mypy errors ignores in test_colours.py

--- tests/test_colours.py
+++ tests/test_colours.py
@@ -86,11 +86,11 @@ def test_errors_of_RGBA():
     with pytest.raises(ValueError):
         RGBA("1", 1, 1, 1)  # wrong type
     with pytest.raises(TypeError):
-        RGBA(1, 1, 1, 1) + RGBA(1, 1, 1, 1)  # type: ignore
+        RGBA(1, 1, 1, 1) + RGBA(1, 1, 1, 1)
     with pytest.raises(TypeError):
-        RGBA(1, 1, 1, 1).append(RGBA(1, 1, 1, 1))  # type: ignore
+        RGBA(1, 1, 1, 1).append(RGBA(1, 1, 1, 1))
     with pytest.raises(TypeError):
-        RGBA(1, 1, 1, 1).extend(RGBA(1, 1, 1, 1))  # type: ignore
+        RGBA(1, 1, 1, 1).extend(RGBA(1, 1, 1, 1))
 
 
 @pytest.mark.parametrize(
@@ -114,8 +114,8 @@ def test_errors_of_RGBA_copy(c: RGBA):
         new[0] = -1  # outside of bounds
     with pytest.raises(IndexError):
         new[4] = 1  # index doesn't exist
-    with pytest.raises(ValueError):
-        new[0] = "1"  # wrong type  # type: ignore
+    with pytest.raises(ValueError):  # wrong type
+        new[0] = "1"  # type: ignore[call-overload]
 
 
 def test_create_palette():
