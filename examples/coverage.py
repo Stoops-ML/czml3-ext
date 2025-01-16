b156660a756fe2e3e67f2fb67ce4a3b2ba5b73dc
Daniel Stoops <dainelst@omnisys.co.il>
2025-01-16 11:01:24 +0200

    Convert example notebooks from jupyter to marimo

---
+++ examples/coverage.py
@@ -0,0 +1,330 @@
+import marimo
+
+__generated_with = "0.10.13"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    import pathlib
+
+    import marimo as mo
+    import numpy as np
+    import rasterio
+    from czml3 import CZML_VERSION, Document, Packet
+    from czml3.properties import (
+        Color,
+        Material,
+        Polygon,
+        PositionList,
+        SolidColorMaterial,
+    )
+
+    import czml3_ext
+    from czml3_ext import packets, rasters
+    from czml3_ext.colours import (
+        RGBA_black,
+        RGBA_white,
+        create_palette,
+    )
+
+    return (
+        CZML_VERSION,
+        Color,
+        Document,
+        Material,
+        Packet,
+        Polygon,
+        PositionList,
+        RGBA_black,
+        RGBA_white,
+        SolidColorMaterial,
+        create_palette,
+        czml3_ext,
+        mo,
+        np,
+        packets,
+        pathlib,
+        rasterio,
+        rasters,
+    )
+
+
+@app.cell
+def _(__file__, pathlib):
+    dir = pathlib.Path(__file__).parent
+    return (dir,)
+
+
+@app.cell
+def _(czml3_ext):
+    print(f"Using czml3-ext version: {czml3_ext.__version__}")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""## Coverage""")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""### Coverage of a single sensor""")
+    return
+
+
+@app.cell
+def _(dir, packets, rasters):
+    vs = packets.coverage(rasters.ops(dir / "data.tif", 5), delete_rasters=True)
+    return (vs,)
+
+
+@app.cell
+def _(dir, rasterio):
+    with rasterio.open(dir / "data.tif") as src:
+        arr = src.read(1)
+        origin_x, origin_y = src.transform * (0, 0)
+        delta_x = src.transform[0]
+        delta_y = src.transform[4]
+    return arr, delta_x, delta_y, origin_x, origin_y, src
+
+
+@app.cell
+def _(arr, delta_x, delta_y, np, origin_x, origin_y, packets):
+    sensor_coverage = packets.sensor(
+        np.array(
+            [
+                [origin_y + arr.shape[0] / 2 * delta_y],
+                [origin_x + arr.shape[1] / 2 * delta_x],
+                [0],
+            ]
+        ),
+        320,
+        20,
+        180,
+        20,
+        10_000,
+    )
+    return (sensor_coverage,)
+
+
+@app.cell
+def _(CZML_VERSION, Document, Packet, sensor_coverage, vs):
+    _doc = Document(
+        packets=[Packet(name="example", id="document", version=CZML_VERSION)]
+        + vs
+        + sensor_coverage
+    )
+    with open("example.czml", "w") as _f:
+        _f.write(_doc.dumps())
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""### Coverage with a hole""")
+    return
+
+
+@app.cell
+def _(dir, packets, rasters):
+    vs_hole = packets.coverage(
+        rasters.ops(dir / "data.tif", 5),
+        rasters.ops(dir / "data_cut.tif", 5),
+        delete_rasters=True,
+    )
+    return (vs_hole,)
+
+
+@app.cell
+def _(CZML_VERSION, Document, Packet, sensor_coverage, vs_hole):
+    _doc = Document(
+        packets=[Packet(name="example", id="document", version=CZML_VERSION)]
+        + vs_hole
+        + sensor_coverage
+    )
+    with open("example.czml", "w") as _f:
+        _f.write(_doc.dumps())
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        ### Coverage above a height
+        Show areas that are above 100 metres.
+        """
+    )
+    return
+
+
+@app.cell
+def _(dir, packets, rasters):
+    vs_height = packets.coverage(
+        rasters.ops(dir / "data.tif", 100, "ge", band=2), delete_rasters=True
+    )
+    return (vs_height,)
+
+
+@app.cell
+def _(CZML_VERSION, Document, Packet, sensor_coverage, vs_height):
+    _doc = Document(
+        packets=[Packet(name="example", id="document", version=CZML_VERSION)]
+        + vs_height
+        + sensor_coverage
+    )
+    with open("example.czml", "w") as _f:
+        _f.write(_doc.dumps())
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        ### Coverage between heights
+        Show areas that are between 100 metres and 200 metres. The method below uses a hole to remove areas below 100 metres.
+        """
+    )
+    return
+
+
+@app.cell
+def _(dir, packets, rasters):
+    vs_heights = packets.coverage(
+        rasters.ops(dir / "data.tif", 200, "le", band=2),
+        rasters.ops(dir / "data.tif", 100, "le", band=2),
+        delete_rasters=True,
+    )
+    return (vs_heights,)
+
+
+@app.cell
+def _(CZML_VERSION, Document, Packet, sensor_coverage, vs_heights):
+    _doc = Document(
+        packets=[Packet(name="example", id="document", version=CZML_VERSION)]
+        + vs_heights
+        + sensor_coverage
+    )
+    with open("example.czml", "w") as _f:
+        _f.write(_doc.dumps())
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""The next method chains operations on the same raster and therefore doesn't require inputting holes to `packets.coverage()`. This is the recommended approach as it is more performant."""
+    )
+    return
+
+
+@app.cell
+def _(dir, packets, rasters):
+    vs_heights_1 = packets.coverage(
+        rasters.ops(dir / "data.tif", [200, 100], ["le", "ge"], band=2),
+        delete_rasters=True,
+    )
+    return (vs_heights_1,)
+
+
+@app.cell
+def _(CZML_VERSION, Document, Packet, sensor_coverage, vs_heights_1):
+    _doc = Document(
+        packets=[Packet(name="example", id="document", version=CZML_VERSION)]
+        + vs_heights_1
+        + sensor_coverage
+    )
+    with open("example.czml", "w") as _f:
+        _f.write(_doc.dumps())
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""`rasters.ops()` will raise an error if the output raster is empty. This can be turned off using the `error_if_no_data` input."""
+    )
+    return
+
+
+@app.cell
+def _(dir, rasters):
+    try:
+        rasters.ops(dir / "data.tif", -999)  # error_if_no_data=True by default
+    except ValueError as e:
+        print(e)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        ### Amount of coverage overlap
+        Show polygons of the amount of overlap. The amount of overlap corresponds to a colour on a predefined scale.
+        """
+    )
+    return
+
+
+@app.cell
+def _(dir, rasters):
+    fpaths = [dir / "data_cut.tif", dir / "data.tif"]
+    fpath_coverage, num_coverages = rasters.coverage_amount(fpaths, 5)
+    return fpath_coverage, fpaths, num_coverages
+
+
+@app.cell
+def _(RGBA_black, RGBA_white, create_palette, fpaths):
+    rgba = create_palette([RGBA_black, RGBA_white], len(fpaths) + 1)
+    return (rgba,)
+
+
+@app.cell
+def _(
+    Color,
+    Material,
+    Polygon,
+    PositionList,
+    SolidColorMaterial,
+    fpath_coverage,
+    num_coverages,
+    packets,
+    rasters,
+    rgba,
+):
+    overlaps = []
+    for num_covers in num_coverages:
+        overlaps.extend(
+            packets.coverage(
+                rasters.ops(fpath_coverage, num_covers),
+                name=f"Overlap of {num_covers}",
+                polygon=Polygon(
+                    positions=PositionList(cartographicDegrees=[0, 0, 0]),
+                    material=Material(
+                        solidColor=SolidColorMaterial(
+                            color=Color(rgba=rgba[num_covers])
+                        )
+                    ),
+                ),
+                delete_rasters=True,
+            )
+        )
+    return num_covers, overlaps
+
+
+@app.cell
+def _(CZML_VERSION, Document, Packet, overlaps):
+    _doc = Document(
+        packets=[Packet(name="example", id="document", version=CZML_VERSION)] + overlaps
+    )
+    with open("example.czml", "w") as _f:
+        _f.write(_doc.dumps())
+    return
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ examples/time_based.py
@@ -0,0 +1,214 @@
+import marimo
+
+__generated_with = "0.10.13"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    import datetime
+
+    import marimo as mo
+    import numpy as np
+    import shapely
+    from czml3 import CZML_VERSION, Document, Packet
+    from czml3.properties import (
+        Clock,
+        Color,
+        Material,
+        Polygon,
+        PositionList,
+        SolidColorMaterial,
+    )
+    from czml3.types import IntervalValue, TimeIntervalCollection
+
+    import czml3_ext
+    from czml3_ext import packets
+    from czml3_ext.colours import RGBA_blue, RGBA_orange, RGBA_white, create_palette
+    from czml3_ext.helpers import get_border
+
+    return (
+        CZML_VERSION,
+        Clock,
+        Color,
+        Document,
+        IntervalValue,
+        Material,
+        Packet,
+        Polygon,
+        PositionList,
+        RGBA_blue,
+        RGBA_orange,
+        RGBA_white,
+        SolidColorMaterial,
+        TimeIntervalCollection,
+        create_palette,
+        czml3_ext,
+        datetime,
+        get_border,
+        mo,
+        np,
+        packets,
+        shapely,
+    )
+
+
+@app.cell
+def _(czml3_ext):
+    print(f"Using czml3-ext version: {czml3_ext.__version__}")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        ## Time
+        czml3-ext works fully with czml3.
+        """
+    )
+    return
+
+
+@app.cell
+def _(packets):
+    border = packets.border("Israel")
+    return (border,)
+
+
+@app.cell
+def _(get_border, np, shapely):
+    ddm_LLA_israel = get_border("israel")
+    poly_shape = shapely.Polygon(ddm_LLA_israel[:, [0, 1], 0].tolist())
+    x = np.linspace(ddm_LLA_israel[:, 1, 0].min(), ddm_LLA_israel[:, 1, 0].max(), 20)
+    y = np.linspace(ddm_LLA_israel[:, 0, 0].min(), ddm_LLA_israel[:, 0, 0].max(), 20)
+    xv, yv = np.meshgrid(x, y)
+    ddm_points = []
+    for _i in range(x.shape[0]):
+        for j in range(y.shape[0]):
+            if not poly_shape.contains(shapely.Point(yv[j, _i], xv[j, _i])):
+                continue
+            ddm_points.append([yv[j, _i], xv[j, _i], 0])
+    ddm_LLA_points = np.array(ddm_points).reshape((-1, 3, 1))
+    return (
+        ddm_LLA_israel,
+        ddm_LLA_points,
+        ddm_points,
+        j,
+        poly_shape,
+        x,
+        xv,
+        y,
+        yv,
+    )
+
+
+@app.cell
+def _(RGBA_blue, RGBA_orange, RGBA_white, create_palette, ddm_LLA_points):
+    rgba = create_palette(
+        [RGBA_blue, RGBA_white, RGBA_orange, RGBA_blue], ddm_LLA_points.shape[0]
+    )
+    return (rgba,)
+
+
+@app.cell
+def _(Color, IntervalValue, datetime, rgba):
+    start = datetime.datetime.now()
+    s_step = 20
+    cc = []
+    for _i in range(len(rgba)):
+        d = []
+        for ii, c in enumerate(rgba[_i:] + rgba[:_i]):
+            d.append(
+                IntervalValue(
+                    start=start + datetime.timedelta(seconds=s_step) * ii,
+                    end=start + datetime.timedelta(seconds=s_step) * (ii + 1),
+                    value=Color(rgba=c),
+                )
+            )
+        cc.append(d)
+    return c, cc, d, ii, s_step, start
+
+
+@app.cell
+def _(
+    Color,
+    Material,
+    Polygon,
+    PositionList,
+    SolidColorMaterial,
+    TimeIntervalCollection,
+    cc,
+    ddm_LLA_israel,
+    ddm_LLA_points,
+    packets,
+    rgba,
+):
+    grid = packets.grid(
+        ddm_LLA_points,
+        polygon=[
+            Polygon(
+                positions=PositionList(cartographicDegrees=[0, 0, 0]),
+                material=Material(
+                    solidColor=SolidColorMaterial(
+                        color=TimeIntervalCollection(values=c)
+                    )
+                ),
+                outlineColor=Color(rgba=[255, 255, 255, 255]),
+                outline=True,
+            )
+            for c in cc
+        ],
+        name=[f"Square {i}" for i in range(len(rgba))],
+        description=[
+            f"Colour: [{c[0]:.2f}, {c[1]:.2f}, {c[2]:.2f}, {c[3]:.2f}]" for c in rgba
+        ],
+        ddm_LLA_cut=ddm_LLA_israel,
+    )
+    return (grid,)
+
+
+@app.cell
+def _(
+    CZML_VERSION,
+    Clock,
+    Document,
+    IntervalValue,
+    Packet,
+    border,
+    datetime,
+    grid,
+    ii,
+    s_step,
+    start,
+):
+    doc = Document(
+        packets=(
+            [
+                Packet(
+                    name="simple",
+                    id="document",
+                    version=CZML_VERSION,
+                    clock=IntervalValue(
+                        start=start,
+                        end=start + datetime.timedelta(seconds=s_step) * (ii + 1),
+                        value=Clock(currentTime=start, multiplier=1200),
+                    ),
+                )
+            ]
+            + border
+            + grid
+        )
+    )
+    return (doc,)
+
+
+@app.cell
+def _(doc):
+    with open("example_time.czml", "w") as f:
+        f.write(doc.dumps())
+    return (f,)
+
+
+if __name__ == "__main__":
+    app.run()
