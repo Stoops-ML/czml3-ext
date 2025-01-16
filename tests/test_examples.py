70eed787cedb7297577020e82203997df122d076
Daniel Stoops <dainelst@omnisys.co.il>
2025-01-16 11:18:12 +0200

    Add test_examples.py

---
+++ tests/test_examples.py
@@ -0,0 +1,12 @@
+import sys
+import subprocess
+import pathlib
+
+def test_examples():
+    dir_examples = pathlib.Path(__file__).parent.parent / "examples"
+    assert dir_examples.exists()
+    for f in dir_examples.iterdir():
+        if f.suffix != ".py":
+            continue
+        result = subprocess.run([sys.executable, f], capture_output=True, text=True)
+        assert result.returncode == 0
